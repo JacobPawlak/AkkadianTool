ac00fca406dfb1c188304a940f943281aec50377
JacobPawlak <jacob.pawlak@uky.edu>
2018-12-05 23:48:50 -0500

    added something to akkadian.js?

--- js/akkadian.js
+++ js/akkadian.js
@@ -3,3 +3,21 @@
 //File: akkadian.js
 //LIN395 - Dr. Andrew Byrd
 
+//this is the javascript file I am going to use in the website,
+// it will be calling in the akkadian.json file, and all of the images
+// associated in the imgs folder
+
+// -------------------- GLOBAL VARIABLES --------------------
+
+var Gleft_side = 0;
+var Gright_side = 0;
+var Gtop_side = 0;
+
+
+
+// -------------------- FUNCTIONS --------------------
+
+
+
+
+// -------------------- COMMENTS AND JOKES --------------------
